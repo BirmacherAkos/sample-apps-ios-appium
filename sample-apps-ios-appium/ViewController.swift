ed8490415b0cf7c41ab0dadce7eadf3df43f0e36
Akos Birmacher <akos.birmacher@bitrise.io>
2018-12-13 17:14:50 +0100

    Initial Commit

---
+++ sample-apps-ios-appium/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample-apps-ios-appium
+//
+//  Created by Akos Birmacher on 2018. 12. 13..
+//  Copyright © 2018. Bitrise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ sample-apps-ios-appiumTests/sample_apps_ios_appiumTests.swift
@@ -0,0 +1,34 @@
+//
+//  sample_apps_ios_appiumTests.swift
+//  sample-apps-ios-appiumTests
+//
+//  Created by Akos Birmacher on 2018. 12. 13..
+//  Copyright © 2018. Bitrise. All rights reserved.
+//
+
+import XCTest
+@testable import sample_apps_ios_appium
+
+class sample_apps_ios_appiumTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
